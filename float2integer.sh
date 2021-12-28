17ea46c2f52f0b776d1fc691c3f3a5acc7c519e9
Bastian Bittorf <bb@npl.de>
2021-12-28 17:25:31 +0100

    add 2 more implementations

--- float2integer.sh
+++ float2integer.sh
@@ -40,13 +40,49 @@ float2integer()
   printf '%s\n' "${out:--1}"
 }
 
+# https://stackoverflow.com/questions/1362298/convert-floating-point-variable-to-integer
+mult1000()
+{
+  local floor=${1%.*}
+  [ -z "$1" ] && echo -1 && return
+  [ "$floor" = 0 ] && floor=
+  local frac=0000
+  [ "$floor" = "$1" ] || frac=${1#*.}$frac
+
+  case "${#frac}" in
+    0|3) ;;
+    1) frac="${frac}00" ;;
+    2) frac="${frac}0" ;;
+    *) frac="${frac%"${frac#???}"}" ;;  # remove overlong tail, keep 3 chars
+  esac
+
+  has_leading_zeroes()
+  {
+    case "$1" in 0[0-9]*) true ;; *) false ;; esac
+  }
+
+  o="${floor}${frac}"
+  while has_leading_zeroes "$o"; do
+    o=${o#?}  # remove first char
+  done
+
+  echo $o
+}
+
 # functionally the same as above, but in a handy (216 bytes) oneliner format:
-x(){ local o r f=${1%.*};r=${1#*.};case $1 in ''|*.*);;*)r=000;esac;case ${#r} in 1)r=${r}00;;2)r=${r}0;;0|3);;*)r=${r%${r#???}};esac;o=${f}$r;while case $o in 0[0-9]*):;;*)false;esac;do o=${o#?};done;echo ${o:--1};}
+x(){ local o r f=${1%.*};r=${1#*.};case $1 in ''|*.*);;*)r=000;esac;case ${#r} in 0|3);;1)r=${r}00;;2)r=${r}0;;*)r=${r%${r#???}};esac;o=${f}$r;while case $o in 0[0-9]*):;;*)false;esac;do o=${o#?};done;echo ${o:--1};}
 
 # functionally the same as above, but does not accept plain integers as input (183 bytes):
-y(){ local o r f=${1%.*};r=${1#*.};case ${#r} in 0|3);;1)r=${r}00;;2)r=${r}0;;*)r=${r%${r#???}};esac;o=${f}$r;while case $o in 0[0-9]*):;;*)false;esac;do o=${o#?};done;echo ${o:--1};}
+y(){ local o r f=${1%.*};r=${1#*.};                                 case ${#r} in 0|3);;1)r=${r}00;;2)r=${r}0;;*)r=${r%${r#???}};esac;o=${f}$r;while case $o in 0[0-9]*):;;*)false;esac;do o=${o#?};done;echo ${o:--1};}
 
-# just for comparing:
+# https://unix.stackexchange.com/questions/89712/how-to-convert-floating-point-number-to-integer
+# using awk:
+use_awk()
+{
+  awk "BEGIN{printf \"%d\", int(${1:--0.001}*1000)}"
+}
+
+# our reference for comparing with the other functions:
 use_bc()
 {
   echo "( ${1:--0.001} * 1000 / 1 )" | bc
@@ -55,15 +91,19 @@ use_bc()
 f()
 {
   local float="$1"
-  local f1 o1 o2 ref
+  local f1 f2 o1 o2 a1 ref
   f1="$( float2integer "$1" )"
+  f2="$( mult1000 "$1" )"
   o1="$( x "$1" )"
   o2="$( y "$1" )"
+  a1="$( use_awk "$1" )"
   ref="$( use_bc "$1" )"		# reference
 
   test "$ref" = "$f1" || echo "ERROR with posix-long for $float => $f1"
+  test "$ref" = "$f2" || echo "ERROR with mult1000 for $float => $f2"
   test "$ref" = "$o1" || echo "ERROR with oneliner1 for $float => $o1"
   test "$ref" = "$o2" || echo "ERROR with oneliner2 for $float => $o2"
+  test "$ref" = "$a1" || echo "ERROR with awkliner for $float => $a1"
 
   printf '%s\n' "input: ${float:-<empty>} => $ref"
 }
